1132bd6914fdd08ead188d45b74f12fac469df45
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-08-10 01:32:53 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.py
+++ Difficulty: Medium/Topological sort/topological-sort.py
@@ -1,25 +1,48 @@
 class Solution:
     
-    def dfs(self, src, adj, vis, stk):
-        vis[src] = True
-        for neigh in adj[src]:
-            if vis[neigh] == False:
-                self.dfs(neigh, adj, vis, stk)
-        stk.append(src)
+    # def dfs(self, src, adj, vis, stk):
+    #     vis[src] = True
+    #     for neigh in adj[src]:
+    #         if vis[neigh] == False:
+    #             self.dfs(neigh, adj, vis, stk)
+    #     stk.append(src)
     
     #Function to return list containing vertices in Topological order.
     def topoSort(self, V, adj):
         # Code here
-        vis = []
-        for i in range(V): vis.append(False)
-        stk = []
+        # vis = []
+        # for i in range(V): vis.append(False)
+        # stk = []
+        # for i in range(V):
+        #     if vis[i] == False:
+        #         self.dfs(i, adj, vis, stk)
+        # path = []
+        # while len(stk) > 0:
+        #     path.append(stk.pop())
+        # return path
+        
+        indegree = {}
+        for i in range(V): indegree[i] = 0
+        for neighs in adj:
+            for i in neighs:
+                indegree[i] += 1
+        que = []
         for i in range(V):
-            if vis[i] == False:
-                self.dfs(i, adj, vis, stk)
-        path = []
-        while len(stk) > 0:
-            path.append(stk.pop())
-        return path
+            if indegree[i] == 0:
+                que.append(i)
+        ans = []
+        while len(que) > 0:
+            size = len(que)
+            for i in range(size):
+                src = que.pop(0)
+                if indegree[src] == 0:
+                    ans.append(src)
+                for neigh in adj[src]:
+                    indegree[neigh] -= 1
+                    if indegree[neigh] == 0:
+                        que.append(neigh)
+        return ans
+                    
 
 
 
